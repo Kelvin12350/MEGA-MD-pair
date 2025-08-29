ebdd4e603c35e283947cab66b6aad60c09126a8e
Heatless <dianwilson66@gmail.com>
2025-08-29 05:56:44 +0100

    Update qr.js

--- qr.js
+++ qr.js
@@ -70,7 +70,7 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "mb~" + string_session;
+                        let md = "md~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `*╭━━━━━━━━━━━━━━━━━━━━━╮
 ┃   MEGA V2 USER   ┃
